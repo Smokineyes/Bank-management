0653d47df34d24f2c7233a749a02f22dddb161b9
Allen Wenduwesun <114917537+Smokineyes@users.noreply.github.com>
2024-06-16 16:35:46 +0300

    Add files via upload

---
+++ Bank Management/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include<sstream>
+#include<string.h>
+using namespace std;
+int main()
+{   char tt;
+    int cus=150,counter=0,entry=0,passw=4321,loo=1,bank=1,user=0,x;
+    float in=0.09, balance[cus]={0},dep,tes;
+    string r,b, acno[cus],name[cus],modif[cus],pass[cus],tr={"Active"},fal={"Inactive"},z,fullname="",au;
+
+ifstream inputfile;
+inputfile.open("details.txt");
+
+return 0;
+}
